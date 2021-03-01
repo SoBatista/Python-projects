4233031d32a16d50b214482ca7c775d7e9461808
SolangeBatista <solangefilipa08@gmail.com>
2021-03-01 16:31:18 +0100

    calculate_the_area_of_a_circle.py

---
+++ Exercises/calculate_the_area_of_a_circle.py
@@ -0,0 +1,9 @@
+"""
+In this exercise I will prompt the user to enter the number of the radius of the circle for me to calculate the area
+"""
+
+radius = raw_input("Enter the number of the radius of the circle so I can calculate the area: ")
+
+area = 3.14 * int(radius) ** 2
+
+print area
