960025519edcfc67d63c294c0fc260c245eaf62d
SolangeBatista <solangefilipa08@gmail.com>
2021-03-01 15:59:33 +0100

    Third practice exercise

---
+++ Exercises/x_hours_correspond_y_days.py
@@ -0,0 +1,9 @@
+"""
+In this exercise I will prompt the user to enter an integer that correspond a certain number of days
+"""
+
+integer = raw_input("Enter the hours to find out how many days it corresponds to: ")
+
+res = float(integer) / 24
+
+print res
