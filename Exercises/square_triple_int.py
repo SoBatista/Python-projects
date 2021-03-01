d83aee989db8a82ad3207243850b47f4f138a86c
SolangeBatista <solangefilipa08@gmail.com>
2021-03-01 12:55:12 +0100

    First Practice exercise square of the triple of an integer added

---
+++ Exercises/square_triple_int.py
@@ -0,0 +1,11 @@
+"""
+This program requests to the user an integer and returns the square of the triple of that integer 
+"""
+
+integer = raw_input("Enter a integer: ")
+
+print integer
+
+res = (int(integer) * 3) ** 2
+
+print res
