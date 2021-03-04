67fd2b6a46ccf8af009cebb53173af56d19e9d87
SolangeBatista <solangefilipa08@gmail.com>
2021-03-04 13:01:33 +0100

    8º practice exercise

---
+++ Exercises/exercise_9.py
@@ -0,0 +1,20 @@
+"""
+This program will prompet the user to enter a number that he want move from degrees centigrade to Fahrenheit and vice versa
+"""
+
+temperature = float(raw_input("Enter a number for me to move it  from degrees centigrade to Fahrenheit and vice versa: "))
+
+ans = raw_input("Enter a f (of Fahrenheit) or a c (of centigrade): " )
+
+if "f" or "F":
+    print temperature * 1.8 + 32, "F"
+
+elif "c" or "C":
+    print (temperature - 32) / 1.8, "C"
+    
+else:
+    print "You did not choose a correct option!"
+
+
+
+
