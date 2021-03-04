e9bfb51941c43391eb870ba0d9b219852e8f194f
SolangeBatista <solangefilipa08@gmail.com>
2021-03-04 10:20:51 +0100

    7º practice exercise

---
+++ Exercises/exercise_7.py
@@ -0,0 +1,34 @@
+""" This program will prompt the user to enter a number and with that number the program will write the multiplication table
+"""
+
+number = int(raw_input("Enter a number forme to write the multiplication table: "))
+
+res = number * 1
+print number, "x 1 =", res
+
+res = number * 2
+print number, "x 2 =", res
+
+res = number * 3
+print number, "x 3 =", res
+
+res = number * 4
+print number, "x 4 =", res
+
+res = number * 5 
+print number, "x 5 =", res
+
+res = number * 6 
+print number, "x 6 =", res
+
+res = number * 7
+print number, "x 7 =", res
+
+res = number * 8 
+print number, "x 8 =", res
+
+res = number * 9 
+print number, "x 9 =", res
+
+res = number * 10
+print number, "x 10 =", res
