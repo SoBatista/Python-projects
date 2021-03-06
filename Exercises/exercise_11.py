8f77eedb85b9e68a9e0efbb09348c4282274298f
SolangeBatista <solangefilipa08@gmail.com>
2021-03-06 15:00:41 +0100

    11º practice exercise

---
+++ Exercises/exercise_11.py
@@ -0,0 +1,6 @@
+def hours_days():
+   arg = float(raw_input("Enter a number of hours and the program will convert to how many days that number of hours corresponds to: "))
+   
+   print arg / 24
+    
+hours_days()
