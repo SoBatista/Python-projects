d36f3a1c7bcf3916c39fdc244275e594ce274d5f
SolangeBatista <solangefilipa08@gmail.com>
2021-03-06 15:01:56 +0100

    11º practice exercise

--- Exercises/exercise_11.py
+++ Exercises/exercise_11.py
@@ -1,3 +1,7 @@
+"""
+This program will prompt the user to enter a number and that it will return in days
+"""
+
 def hours_days():
    arg = float(raw_input("Enter a number of hours and the program will convert to how many days that number of hours corresponds to: "))
    
