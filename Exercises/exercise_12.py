d98b6f53948f78fd9daae0514d2eee5499e35159
SolangeBatista <solangefilipa08@gmail.com>
2021-03-08 21:01:08 +0100

    12º practice exercise

--- Exercises/exercise_12.py
+++ Exercises/exercise_12.py
@@ -1,3 +1,7 @@
+"""
+This program will prompt the user to enter the first three lowercase letters of a month of his choice. Then the program will return how many days that month has.
+"""
+
 
 def days_months(arg):
     if arg == "jan" or arg == "mar" or arg == "may" or arg == "jul" or arg == "aug" or arg == "oct" or arg == "dec":
@@ -9,7 +13,7 @@ def days_months(arg):
     else:
         print "Value Error"
 
-
+print "Enter the first three letters of a month of your choice"
 arg = raw_input("Give me a month: ")
 days_months(arg)
     
