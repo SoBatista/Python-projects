f263f091b5498e432eaaa5b2a836b77064dfe5f2
SoBatista <solangefilipa08@gmail.com>
2022-08-19 14:54:08 +0200

    inverted numbers

---
+++ Exercises/ex_24.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""
+This program requests to the user to enter a positive integer and prints the digits of that number inverted
+"""
+
+num=int(input("Enter a positive integer: "))
+print(str(num)[::-1])
